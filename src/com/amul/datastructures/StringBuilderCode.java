fe0e06cee8de96fd90fdfecdd17624b6bc024fa4
Amul Gaurav <amulgaurav003@gmail.com>
2024-08-09 12:27:13 +0530

    Add StringBuilder code

--- src/com/amul/datastructures/StringBuilderCode.java
+++ src/com/amul/datastructures/StringBuilderCode.java
@@ -1,4 +1,14 @@
 package com.amul.datastructures;
 
-public class StringBuilder {
+public class StringBuilderCode {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < 26; i++) {
+            char ch = (char)('a' + i);
+            builder.append(ch);
+        }
+
+        System.out.println(builder);
+    }
 }
