9dfb3637fe882d98aaceb33ec60bf7acdd1bc36a
Amul Gaurav <amulgaurav003@gmail.com>
2024-09-10 23:45:13 +0530

    Add insert method using recursion

--- src/com/amul/datastructures/LL.java
+++ src/com/amul/datastructures/LL.java
@@ -54,6 +54,22 @@ public class LL {
         size++;
     }
 
+    public void insertRec(int val, int index) {
+        head = insertRec(val, index, head);
+    }
+
+    private Node insertRec(int val, int index, Node node) {
+        if (index == 0) {
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+
+        node.next = insertRec(val, index - 1, node.next);
+
+        return node;
+    }
+
     public int deleteFirst() {
         int val = head.value;
         head = head.next;
@@ -91,6 +107,7 @@ public class LL {
         int val = prev.next.value;
 
         prev.next = prev.next.next;
+        size--;
 
         return val;
     }
